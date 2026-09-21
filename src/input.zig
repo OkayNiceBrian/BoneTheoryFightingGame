cd92d05c1df10d2c85398ea5bdd88b3e1219ed48
Brian Jackson <brianjackson1224@gmail.com>
2026-09-21 15:36:45 -0700

    change inputHandler to PlayerInputHandler and figure out controls

--- src/input.zig
+++ src/input.zig
@@ -1,19 +1,35 @@
 const rl = @import("raylib");
 
-pub const InputHandler = struct {
+pub const PlayerInputHandler = struct {
+    // default button binds
+    leftButton: rl.KeyboardKey = rl.KeyboardKey.a,
+    rightButton: rl.KeyboardKey = rl.KeyboardKey.d,
+    upButton: rl.KeyboardKey = rl.KeyboardKey.w,
+    downButton: rl.KeyboardKey = rl.KeyboardKey.s,
+    punchButton: rl.KeyboardKey = rl.KeyboardKey.u,
+    kickButton: rl.KeyboardKey = rl.KeyboardKey.j,
+    mediumButton: rl.KeyboardKey = rl.KeyboardKey.i,
+    heavyButton: rl.KeyboardKey = rl.KeyboardKey.k,
+    uniqueButton: rl.KeyboardKey = rl.KeyboardKey.o,
+    assistButton: rl.KeyboardKey = rl.KeyboardKey.l,
+    defendButton: rl.KeyboardKey = rl.KeyboardKey.p,
+    throwButton: rl.KeyboardKey = rl.KeyboardKey.semicolon,
+    selectButton: rl.KeyboardKey = rl.KeyboardKey.left_shift,
+    startButton: rl.KeyboardKey = rl.KeyboardKey.escape,
+
     // pressed buttons
     leftPressed: bool = false,
     rightPressed: bool = false,
     upPressed: bool = false,
     downPressed: bool = false,
-    aPressed: bool = false,
-    bPressed: bool = false,
-    xPressed: bool = false,
-    yPressed: bool = false,
-    lPressed: bool = false,
-    rPressed: bool = false,
-    zlPressed: bool = false,
-    zrPressed: bool = false,
+    punchPressed: bool = false,
+    kickPressed: bool = false,
+    mediumPressed: bool = false,
+    heavyPressed: bool = false,
+    uniquePressed: bool = false,
+    assistPressed: bool = false,
+    defendPressed: bool = false,
+    throwPressed: bool = false,
     startPressed: bool = false,
     selectPressed: bool = false,
 
@@ -22,14 +38,14 @@ pub const InputHandler = struct {
     rightTapped: bool = false,
     upTapped: bool = false,
     downTapped: bool = false,
-    aTapped: bool = false,
-    bTapped: bool = false,
-    xTapped: bool = false,
-    yTapped: bool = false,
-    lTapped: bool = false,
-    rTapped: bool = false,
-    zlTapped: bool = false,
-    zrTapped: bool = false,
+    punchTapped: bool = false,
+    kickTapped: bool = false,
+    mediumTapped: bool = false,
+    heavyTapped: bool = false,
+    uniqueTapped: bool = false,
+    assistTapped: bool = false,
+    defendTapped: bool = false,
+    throwTapped: bool = false,
     startTapped: bool = false,
     selectTapped: bool = false,
 
@@ -41,60 +57,132 @@ pub const InputHandler = struct {
 
     // TODO: Make key bindings dynamic
     fn handlePresses(self: *@This()) void {
-        if (rl.isKeyPressed(rl.KeyboardKey.left)) {
+        if (rl.isKeyPressed(self.leftButton)) {
             self.leftPressed = true;
             self.leftTapped = true;
         }
 
-        if (rl.isKeyPressed(rl.KeyboardKey.right)) {
+        if (rl.isKeyPressed(self.rightButton)) {
             self.rightPressed = true;
             self.rightTapped = true;
         }
 
-        if (rl.isKeyPressed(rl.KeyboardKey.up)) {
+        if (rl.isKeyPressed(self.upButton)) {
             self.upPressed = true;
             self.upTapped = true;
         }
 
-        if (rl.isKeyPressed(rl.KeyboardKey.down)) {
+        if (rl.isKeyPressed(self.downButton)) {
             self.downPressed = true;
             self.downTapped = true;
         }
 
-        if (rl.isKeyPressed(rl.KeyboardKey.space)) {
-            self.aPressed = true;
-            self.aTapped = true;
+        if (rl.isKeyPressed(self.punchButton)) {
+            self.punchPressed = true;
+            self.punchTapped = true;
         }
 
-        if (rl.isKeyPressed(rl.KeyboardKey.left_shift)) {
-            self.bPressed = true;
-            self.bTapped = true;
+        if (rl.isKeyPressed(self.kickButton)) {
+            self.kickPressed = true;
+            self.kickTapped = true;
+        }
+
+        if (rl.isKeyPressed(self.mediumButton)) {
+            self.mediumPressed = true;
+            self.mediumTapped = true;
+        }
+
+        if (rl.isKeyPressed(self.heavyButton)) {
+            self.heavyPressed = true;
+            self.heavyTapped = true;
+        }
+
+        if (rl.isKeyPressed(self.uniqueButton)) {
+            self.uniquePressed = true;
+            self.uniqueTapped = true;
+        }
+
+        if (rl.isKeyPressed(self.assistButton)) {
+            self.assistPressed = true;
+            self.assistTapped = true;
+        }
+
+        if (rl.isKeyPressed(self.defendButton)) {
+            self.defendPressed = true;
+            self.defendTapped = true;
+        }
+
+        if (rl.isKeyPressed(self.throwButton)) {
+            self.throwPressed = true;
+            self.throwTapped = true;
+        }
+
+        if (rl.isKeyPressed(self.selectButton)) {
+            self.selectPressed = true;
+            self.selectTapped = true;
+        }
+
+        if (rl.isKeyPressed(self.startButton)) {
+            self.startPressed = true;
+            self.startTapped = true;
         }
     }
 
     fn handleReleases(self: *@This()) void {
-        if (rl.isKeyReleased(rl.KeyboardKey.left)) {
+        if (rl.isKeyReleased(self.leftButton)) {
             self.leftPressed = false;
         }
 
-        if (rl.isKeyReleased(rl.KeyboardKey.right)) {
+        if (rl.isKeyReleased(self.rightButton)) {
             self.rightPressed = false;
         }
 
-        if (rl.isKeyReleased(rl.KeyboardKey.up)) {
+        if (rl.isKeyReleased(self.upButton)) {
             self.upPressed = false;
         }
 
-        if (rl.isKeyReleased(rl.KeyboardKey.down)) {
+        if (rl.isKeyReleased(self.downButton)) {
             self.downPressed = false;
         }
 
-        if (rl.isKeyReleased(rl.KeyboardKey.space)) {
-            self.aPressed = false;
+        if (rl.isKeyReleased(self.punchButton)) {
+            self.punchPressed = false;
         }
 
-        if (rl.isKeyReleased(rl.KeyboardKey.left_shift)) {
-            self.bPressed = false;
+        if (rl.isKeyReleased(self.kickButton)) {
+            self.kickPressed = false;
+        }
+
+        if (rl.isKeyReleased(self.mediumButton)) {
+            self.mediumPressed = false;
+        }
+
+        if (rl.isKeyReleased(self.heavyButton)) {
+            self.heavyPressed = false;
+        }
+
+        if (rl.isKeyReleased(self.uniqueButton)) {
+            self.uniquePressed = false;
+        }
+
+        if (rl.isKeyReleased(self.assistButton)) {
+            self.assistPressed = false;
+        }
+
+        if (rl.isKeyReleased(self.defendButton)) {
+            self.defendPressed = false;
+        }
+
+        if (rl.isKeyReleased(self.throwButton)) {
+            self.throwPressed = false;
+        }
+
+        if (rl.isKeyReleased(self.selectButton)) {
+            self.selectPressed = false;
+        }
+
+        if (rl.isKeyReleased(self.startButton)) {
+            self.startPressed = false;
         }
     }
 
@@ -103,14 +191,14 @@ pub const InputHandler = struct {
         self.rightTapped = false;
         self.upTapped = false;
         self.downTapped = false;
-        self.aTapped = false;
-        self.bTapped = false;
-        self.xTapped = false;
-        self.yTapped = false;
-        self.lTapped = false;
-        self.rTapped = false;
-        self.zlTapped = false;
-        self.zrTapped = false;
+        self.punchTapped = false;
+        self.kickTapped = false;
+        self.mediumTapped = false;
+        self.heavyTapped = false;
+        self.uniqueTapped = false;
+        self.assistTapped = false;
+        self.defendTapped = false;
+        self.throwTapped = false;
         self.startTapped = false;
         self.selectTapped = false;
     }
